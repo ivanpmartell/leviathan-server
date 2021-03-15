95a57853ee317fe02ddb84a46b4268189e1a4db1
Ivan Perez <ivan.pmartell@hotmail.com>
2021-03-15 01:59:28 -0700

    Server listens for packets

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+
+namespace leviathan_server
+{
+    class Program
+    {
+        private const int listenPort = 16700;
+
+        private void StartListener()
+        {
+            TcpListener server = null;
+            try
+            {
+                // Set the TcpListener on port intTemp.
+                Int32 intPort = listenPort;
+                IPAddress localAddr = IPAddress.Parse("0.0.0.0");
+                // TcpListener server = new TcpListener(port);
+                server = new TcpListener(localAddr, intPort);
+                // Start listening for client requests.
+                server.Start();
+                // Buffer for reading data
+                Byte[] bytes = new Byte[4096];
+                 
+                String data = null;
+                // Enter the listening loop.
+                while (true)
+                {
+                    Console.WriteLine("Listening on {0}... ", intPort);
+                    TcpClient client = server.AcceptTcpClient(); // server.AcceptSocket() also good
+                    Console.WriteLine("> Connected to {0}", client.Client.RemoteEndPoint);
+                    data = null;
+                    NetworkStream stream = client.GetStream();
+
+                    int intLoop;
+                    while ((intLoop = stream.Read(bytes, 0, bytes.Length)) != 0)
+                    {
+                        // Translate data bytes to a ASCII string.
+                        data = System.Text.Encoding.ASCII.GetString(bytes, 0, intLoop);
+                        Console.WriteLine("Received: {0} from {1}", data, client.Client.RemoteEndPoint);
+                        // Process the data sent by the client.
+                        data = data.ToUpper();
+                        if (data == "Q") return;
+                        byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+                        // Send back a response.                        
+                        stream.Write(msg, 0, msg.Length);                        
+                        Console.WriteLine("Sent: {0} to {1}", data, client.Client.RemoteEndPoint);
+                    }
+                    client.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Program prog = new Program();
+            prog.StartListener();
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # leviathan-server
+
+Compile using Visual Studio Code (VS Code). This project targets .Net Core 2.1. I am using Ubuntu 18.04.
+
+Install .Net Core using the following [instructions](https://docs.microsoft.com/en-us/dotnet/core/install/linux-scripted-manual#scripted-install).
+
+Here is the script parameters I used for development purposes:
+
+```bash dotnet-install.sh -c 2.1```
+
+Then I added it to the PATH.
+
+To run use ```dotnet run```
+
+To build dll use ```dotnet publish -c Release -r linux-x64 --self-contained```
+
+If you are just running the program on a server, use the complete published folder (removing the .pdb file):
+
+```./publish/leviathan-server```
